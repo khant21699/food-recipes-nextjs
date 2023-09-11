5364a80bc3ccef9007950818f2cf330c7e0a8692
khant21699 <khant21699@hotmail.com>
2023-09-12 00:16:17 +0630

    first push

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["www.themealdb.com"], // Add the domain(s) from which you are loading images
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
