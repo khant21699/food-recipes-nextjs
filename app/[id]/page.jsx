62db58f7ec664c0088ef4929ad8b27b1f62d2ac9
khant21699 <khant21699@hotmail.com>
2023-09-12 00:53:25 +0630

    bug fixes3

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -8,27 +8,18 @@ const getData = async (param, id) => {
       `https://www.themealdb.com/api/json/v1/1/filter.php?c=${id}`,
       { next: { revalidate: 0 } }
     );
-    if (!res.ok) {
-      notFound();
-    }
     return res.json();
   } else if (param == "Country") {
     const res = await fetch(
       `https://www.themealdb.com/api/json/v1/1/filter.php?a=${id}`,
       { next: { revalidate: 0 } }
     );
-    if (!res.ok) {
-      notFound();
-    }
     return res.json();
   } else if (param == "Search") {
     const res = await fetch(
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`,
       { next: { revalidate: 0 } }
     );
-    if (!res.ok) {
-      notFound();
-    }
     return res.json();
   }
 };
@@ -38,43 +29,35 @@ export default async function page({ params }) {
   var parts = id.split("%2B");
   var before = parts[0];
   var after = parts[1];
-  // if (after == null) {
-  //   notFound();
-  // }
+  if (after == null) {
+    notFound();
+  }
   const result = await getData(before, after);
-  // if (result.meals == null) {
-  //   notFound();
-  // }
+  if (result.meals == null) {
+    notFound();
+  }
   return (
     <div className="flex flex-col gap-4">
-      {result !== undefined &&
-        result.meals.map((m, index) => {
-          return (
-            <Link href={`/recipes/${m.idMeal}`}>
-              <div className=" h-[250px] px-4 items-center gap-10 flex  border-green-400 border-[2px]">
-                <div className=" w-2/5 aspect-square imgcontainer md:w-[200px]">
-                  <Image
-                    src={`${m.strMealThumb}/preview`}
-                    quality={100}
-                    alt="foodImg"
-                    layout="responsive"
-                    className=" object-cover"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-                <h3 className="w-3/5 text-green-500">{m.strMeal}</h3>
+      {result.meals.map((m, index) => {
+        return (
+          <Link href={`/recipes/${m.idMeal}`}>
+            <div className=" h-[250px] px-4 items-center gap-10 flex  border-green-400 border-[2px]">
+              <div className=" w-2/5 aspect-square imgcontainer md:w-[200px]">
+                <Image
+                  src={`${m.strMealThumb}/preview`}
+                  quality={100}
+                  alt="foodImg"
+                  layout="responsive"
+                  className=" object-cover"
+                  width={100}
+                  height={100}
+                />
               </div>
-            </Link>
-          );
-        })}
-      {result == undefined && (
-        <div className="w-full text-green-500 text-center">
-          <h1>
-            Sorry, Cannot Find the Page.{before} {after}
-          </h1>
-        </div>
-      )}
+              <h3 className="w-3/5 text-green-500">{m.strMeal}</h3>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
